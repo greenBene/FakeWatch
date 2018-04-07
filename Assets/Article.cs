9d6d37622f4062754fe2360215a8da3e95bb051d
greenBene <benebeckermann@gmail.com>
2018-04-07 21:50:12 +0200

    fixed stuff

--- Assets/Article.cs
+++ Assets/Article.cs
@@ -67,7 +67,6 @@ public class Article : MonoBehaviour {
         bool correct = newsGeneration.Answer(isFake, true);
         if(correct)
         {
-
             Destroy(gameObject);
         } else
         {
