d79c7db7595dd13fcd04d9ec2b45173a6f8baa43
Daniel Berndt <daniel.berndt@gmail.com>
2018-04-08 00:30:23 +0200

    Some working version of FactsFinder

---
+++ Assets/Facts/Editor/FactsTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FactsTest
+{
+
+  [Test]
+  public void FactsTestSimplePasses()
+  {
+    Facts facts = new Facts();
+    facts.init("Assets/nodes.txt");
+    facts.findValid(new List<string> { "Z", "O", "P", "D", "WK" }, new Dictionary<string, string>());
+
+    Assert.AreEqual(1, 1);
+  }
+
+}
