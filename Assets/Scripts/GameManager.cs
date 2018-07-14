02e4d515b016176b6ef4e5e3e1123969346bb7b9
Andreas <andreas.edmeier@web.de>
2018-07-14 21:14:53 +0200

    State Maschine hinzugefügt

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState {
+    Desktop = 0,
+    Tutorial,
+    Playing,
+    EndScreen
+}
+
 public class GameManager : MonoBehaviour {
 
+    //===== ===== Variables ===== =====
     private static GameManager s_instance;
     public static GameManager Instance
     {
@@ -26,13 +34,120 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    // Use this for initialization
+
+
+    GameState state = GameState.Desktop;
+    public bool TutorialFinished = false; 
+
+    //===== ===== Start/Update ===== =====
     void Start () {
-        s_newsSource = GameObject.Find("MainScreen").GetComponent<NewsGeneration>();	
+        s_newsSource = this.GetComponent<NewsGeneration>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        StateTransition();
+        //StateOnStay(); //for stuff that has to be done every frame in an specifik state
 	}
+
+    //===== ===== Registration ===== =====
+    public Timer timer = null;
+    public void RegistUI(Timer handle) {
+        if (timer == handle)
+            timer = null;
+        else
+            timer = handle;
+    }
+
+    //===== ===== State Mashine ===== =====
+    void StateTransition() {
+
+        //----- Any State -----
+        //Quit button
+
+        switch (state) {
+        case GameState.Desktop:
+            //TODO change to Tutorial by klicking an icon
+            break;
+        case GameState.Tutorial:
+            //TODO change to Desktiop by abort
+            if (TutorialFinished)
+                ChangeState(GameState.Playing);
+            break;
+        case GameState.Playing:
+            //TODO change to Desktop by pressing esc
+            if (Input.GetButtonUp(StringCollection.CANCEL))
+                ChangeState(GameState.Desktop);
+            break;
+        case GameState.EndScreen:
+            break;
+        default:
+            break;
+        }
+    }
+
+    void StateOnStay() {
+        switch (state) {
+        case GameState.Desktop:
+            break;
+        case GameState.Tutorial:
+            break;
+        case GameState.Playing:
+            break;
+        case GameState.EndScreen:
+            break;
+        default:
+            break;
+        }
+    }
+
+    void ChangeState(GameState newState) {
+        if (state == newState)
+            return;
+
+        switch (state) { //by exiting this state
+        case GameState.Desktop:
+            break;
+        case GameState.Tutorial:
+            break;
+        case GameState.Playing:
+            if (timer)
+                timer.state = TimerState.TimeShort;
+            else
+                Debug.LogError("timer not found. GameManager");
+            break;
+        case GameState.EndScreen:
+            break;
+        default:
+            break;
+        }
+
+        state = newState;
+
+        switch (state) { //by entering this state
+        case GameState.Desktop:
+            break;
+        case GameState.Tutorial:
+            break;
+        case GameState.Playing:
+            if (timer)
+                timer.state = TimerState.Countdown;
+            else
+                Debug.LogError("timer not found. GameManager");
+            break;
+        case GameState.EndScreen:
+            break;
+        default:
+            break;
+        }
+    }
+
+    public bool RequestStateChange(GameState newState) {
+        if ((state == GameState.Playing && newState == GameState.Tutorial) ||
+            (state == GameState.Tutorial && newState == GameState.EndScreen) ||
+            (state == GameState.Desktop && newState == GameState.EndScreen))
+            return false;
+
+        ChangeState(newState);
+        return true;
+    }
 }
---
+++ Assets/Scripts/StringCollection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringCollection {
+    public const string GAMEMANAGER = "GameManager";
+
+    //Input
+    public const string VERTICAL = "Vertical";
+    public const string HORIZONTAL = "Horizontal";
+    public const string CANCEL = "Cancel";
+
+    //TAGS
+
+    //Sceens
+}
