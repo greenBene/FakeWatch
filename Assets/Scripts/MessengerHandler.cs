7dbdcbb37d0ef823ded6527fab2b72a14d03e933
Unknown <andreas.edmeier@web.de>
2018-07-24 20:46:27 +0200

    An übergabe der Messages rum experementiert

--- Assets/Scripts/MessengerHandler.cs
+++ Assets/Scripts/MessengerHandler.cs
@@ -7,14 +7,17 @@ public class MessengerHandler : MonoBehaviour {
     [SerializeField] GameObject MessengerPrefab;
 
     List<MessengerWindow> WindowList;
+    Dictionary<Inconsistency, string[]> Messages;
 
     private void Start() {
         WindowList = new List<MessengerWindow>();
+        //TODO: Phase informations into Messages Dictionatry
     }
 
     public void NewMessage(Inconsistency handle = null) {
         MessengerWindow newMessenger = Instantiate(MessengerPrefab, GameManager.MainScreen.transform).GetComponent<MessengerWindow>();
         float moveHight = newMessenger.Show("Dummy Message")/2;//TODO: richtige Message übergeben
+        //float moveHight = newMessenger.Show(string.Format(Messages[handle][Random.Range(0, Messages[handle].Length)], handle.info1.value, handle.info2.value) / 2;
 
         foreach (MessengerWindow it in WindowList) {
             it.SlideUp(moveHight);
