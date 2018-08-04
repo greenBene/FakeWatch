e7270f3edf83d1b87399160f6daea695c4c04d6c
Andreas <andreas.edmeier@web.de>
2018-08-04 15:23:36 +0200

    Volumen regler gecodet

---
+++ Assets/Scripts/VolumeHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class VolumeHandler : MonoBehaviour {
+
+    Slider slider;
+
+    private void Start() {
+        slider = GetComponent<Slider>();
+    }
+
+    public void ChangeVolume() {
+        AudioListener.volume = slider.value;
+    }
+}
