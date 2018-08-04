65e50073d78e63f7f24e43befa9368931e814653
Andreas <andreas.edmeier@web.de>
2018-08-04 22:51:43 +0200

    GetValue bei XMLLoader hinzugefügt

--- Assets/Scripts/Language/XMLLoader.cs
+++ Assets/Scripts/Language/XMLLoader.cs
@@ -30,6 +30,30 @@ public class XMLLoader{
         return nodesDict[type];
     }
 
+    public string GetValue(xmlFiles type, string attrelement, string[] key, string[] value) {
+        if(key.Length != value.Length) {
+            new System.Exception("Key value pairs not assignable");
+        }
+
+        string SearchString = "//" + attrelement + "[";
+        for(int i = 0; i < key.Length-1; i++) {
+            SearchString += "@" + key[i] + "='" + value[i] + "' and ";
+        }
+        SearchString += "@" + key[key.Length-1] + "='" + value[value.Length-1] + "']";
+
+        Debug.Log("SerchString is: " + SearchString);
+
+        XmlNodeList nodes = nodesDict[type].SelectNodes(SearchString);
+        int numElements = nodes.Count;
+        if (numElements == 0) {
+            return string.Format("No {0}-element found in {1}", attrelement, SearchString);
+        } else if (numElements > 1) {
+            return string.Format("Multiple {0}-elements found in {1}", attrelement, SearchString);
+        } else {
+            return nodes[0].InnerText;
+        }
+    }
+
     public void UpdateLanguage(bool updateScene)
     {
         nodesDict = new Dictionary<xmlFiles, XmlNode>();
