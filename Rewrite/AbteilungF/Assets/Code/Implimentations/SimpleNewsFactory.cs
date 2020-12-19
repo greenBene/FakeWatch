f39759a295312e3808b25f8dbd8b5e6df5fdaa66
Andreas Edmeier <andreas.edmeier@ubisoft.com>
2020-12-19 19:23:46 +0100

    rewrite seleton implimentation of SimpleNewsFactory

--- Rewrite/AbteilungF/Assets/Code/Implimentations/SimpleNewsFactory.cs
+++ Rewrite/AbteilungF/Assets/Code/Implimentations/SimpleNewsFactory.cs
@@ -2,20 +2,40 @@
 
 public class SimpleNewsFactory : INewsFactory
 {
-	private News myNews;
+	private News myNewsPrototype;
+
+	public SimpleNewsFactory()
+	{
+
+	}
 
 	public News GetNextNews(ILocalisator aLocalisator)
 	{
-		throw new System.NotImplementedException();
+		return GetNextNews(new List<newsElement> {
+				newsElement.areaOfExpertise,
+				newsElement.autor,
+				newsElement.date,
+				newsElement.newspaper,
+				newsElement.place,
+				newsElement.title},
+			aLocalisator);
 	}
 
 	public News GetNextNews(List<newsElement> aContainsElements, ILocalisator aLocalisator)
 	{
-		throw new System.NotImplementedException();
+		var keys = new Dictionary<newsElement, string>();
+		News news = new News();
+
+		foreach(var it in aContainsElements) {
+			keys[it] = ""; // TODO: find a key
+		}
+
+		news.Setup(keys, aLocalisator);
+		return news;
 	}
 
 	public void SetNewsPrototype(News aNews)
 	{
-		myNews = aNews;
+		myNewsPrototype = aNews;
 	}
 }
