a517829ebc0539318e2eac586d1eb7bb1fa5757d
Andreas Edmeier <andreas.edmeier@web.de>
2020-12-28 18:07:41 +0100

    rewrite added auto Localisator

---
+++ Rewrite/AbteilungF/Assets/Code/Localisation/ImageLocalisator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AbteilungF
+{
+	[RequireComponent(typeof(Image))]
+	public class ImageLocalisator : MonoBehaviour
+	{
+		[SerializeField] string myKey;
+		Image myIcon;
+
+		private void Start()
+		{
+			myIcon = GetComponent<Image>();
+			Data.GetInstance().myLanguage.OnValueChangeWithState += UpdateIcon;
+			UpdateIcon(Data.GetInstance().myLanguage.value);
+		}
+
+		void UpdateIcon(language aLanguage)
+		{
+			myIcon.sprite = Data.GetInstance().myLocalisator.GetLocaSprite(aLanguage, myKey);
+		}
+	}
+}
---
+++ Rewrite/AbteilungF/Assets/Code/Localisation/StringLocalisator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace AbteilungF
+{
+	[RequireComponent(typeof(TextMeshProUGUI))]
+	public class StringLocalisator : MonoBehaviour
+	{
+		[SerializeField] string myKey;
+		TextMeshProUGUI myIcon;
+
+		private void Start()
+		{
+			myIcon = GetComponent<TextMeshProUGUI>();
+			Data.GetInstance().myLanguage.OnValueChangeWithState += UpdateIcon;
+			UpdateIcon(Data.GetInstance().myLanguage.value);
+		}
+
+		void UpdateIcon(language aLanguage)
+		{
+			myIcon.text = Data.GetInstance().myLocalisator.GetLocaString(aLanguage, myKey);
+		}
+	}
+}
