e2a5d3c83590368dd8b78e07b3a4ad297782e5de
Andreas Edmeier <andreas.edmeier@web.de>
2020-12-28 18:08:11 +0100

    rewrite added helper classes

---
+++ Rewrite/AbteilungF/Assets/Code/PositionButtons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionButtons : MonoBehaviour
+{
+	[SerializeField] float mySpacing;
+
+	int myChildCount = 0;
+
+	private void FixedUpdate()
+	{
+		if(transform.childCount == myChildCount) {
+			return;
+		}
+		myChildCount = transform.childCount;
+
+		float nextPosition = 0;
+		foreach(RectTransform it in transform) {
+			it.pivot = new Vector2(1, 0.5f);
+			it.anchoredPosition = new Vector2(-nextPosition, 0);
+			nextPosition += it.sizeDelta.x + mySpacing;
+		}
+	}
+}
---
+++ Rewrite/AbteilungF/Assets/Code/Tools/FitToText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace AsserTOOLres
+{
+	[RequireComponent(typeof(TextMeshProUGUI))]
+	public class FitToText : MonoBehaviour
+	{
+		TextMeshProUGUI myText;
+
+		private void Start()
+		{
+			myText = GetComponent<TextMeshProUGUI>();
+		}
+
+		private void FixedUpdate()
+		{
+			((RectTransform)transform).sizeDelta = new Vector2(((RectTransform)transform).sizeDelta.x, myText.preferredHeight);
+		}
+	}
+}
